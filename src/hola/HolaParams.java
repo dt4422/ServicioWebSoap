6ed7773383c013b8fbe0179fba8837be183653d0
dt4422 <danitorres2244@gmail.com>
2017-12-28 12:14:54 +0100

    Practica 8

---
+++ src/hola/HolaParams.java
@@ -0,0 +1,8 @@
+package hola;
+
+public class HolaParams {
+	public String decirHola(String param){
+		System.out.println("voy a deci hola"+param);
+		return "hola mundo"+param;
+	}
+}
---
+++ src/hola/HolaParamsCallbackHandler.java
@@ -0,0 +1,66 @@
+
+/**
+ * HolaParamsCallbackHandler.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
+ */
+
+    package hola;
+
+    /**
+     *  HolaParamsCallbackHandler Callback class, Users can extend this class and implement
+     *  their own receiveResult and receiveError methods.
+     */
+    public abstract class HolaParamsCallbackHandler{
+
+
+
+    protected Object clientData;
+
+    /**
+    * User can pass in any object that needs to be accessed once the NonBlocking
+    * Web service call is finished and appropriate method of this CallBack is called.
+    * @param clientData Object mechanism by which the user can pass in user data
+    * that will be avilable at the time this callback is called.
+    */
+    public HolaParamsCallbackHandler(Object clientData){
+        this.clientData = clientData;
+    }
+
+    /**
+    * Please use this constructor if you don't want to set any clientData
+    */
+    public HolaParamsCallbackHandler(){
+        this.clientData = null;
+    }
+
+    /**
+     * Get the client data
+     */
+
+     public Object getClientData() {
+        return clientData;
+     }
+
+        
+           /**
+            * auto generated Axis2 call back method for decirHola method
+            * override this method for handling normal response from decirHola operation
+            */
+           public void receiveResultdecirHola(
+                    hola.HolaParamsStub.DecirHolaResponse result
+                        ) {
+           }
+
+          /**
+           * auto generated Axis2 Error handler
+           * override this method for handling error response from decirHola operation
+           */
+            public void receiveErrordecirHola(java.lang.Exception e) {
+            }
+                
+
+
+    }
+    
